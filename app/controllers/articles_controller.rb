26528f2962df75486a9f5444a1265ec7789d5272
BraianStrak <braianstrak1999@gmail.com>
2019-09-01 22:38:18 +0100

    Added forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,40 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
 
-
     def show
-        @article = Article.find(params[:id]);
+        @article = Article.find(params[:id]); #show.html.erb for the template
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to article_path(index)
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+      
+        redirect_to article_path(@article)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 end
 
 Blogger::Application.routes.draw do
+  root to: 'articles#index'
   resources :articles
 end
 
