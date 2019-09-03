6e502825eda081c32786dd3a015a745d461703ba
BraianStrak <braianstrak1999@gmail.com>
2019-09-03 23:43:12 +0100

    I4 done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_175926) do
+ActiveRecord::Schema.define(version: 2019_09_03_213236) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
